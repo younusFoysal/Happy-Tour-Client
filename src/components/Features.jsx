468df9dd74c4b4c5158ed6414a847d6550635876
younusFoysal <younus.faysal@gmail.com>
2024-05-05 16:57:32 +0600

    Features Added

---
+++ src/components/Features.jsx
@@ -0,0 +1,127 @@
+import React from 'react';
+
+const Features = () => {
+    return (
+        <div className="carousel-item ">
+
+            <section className=" pb-6">
+                <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
+                    <div className="container mx-auto px-6 p-6">
+
+
+                        <div className="mb-16 text-center">
+                            <h4 className="text-base text-emerald-600 font-semibold tracking-wide uppercase">Features</h4>
+                            <p className="mt-2 text-5xl lg:text-7xl font-bold tracking-tight text-gray-900 dark:text-gray-400">How
+                                we
+                                change the game
+
+                            </p>
+                        </div>
+
+
+                        <div className="flex flex-wrap my-12">
+
+
+                            <div className="w-full border-b md:w-1/2 md:border-r lg:w-1/3 p-8">
+                                <div className="flex items-center mb-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20"
+                                         fill="currentColor" className="h-6 w-6 text-emerald-500">
+                                        <path
+                                            d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z">
+                                        </path>
+                                    </svg>
+                                    <div className="ml-4 text-xl">Increase sales</div>
+                                </div>
+                                <p className="leading-loose text-gray-500">We offer the best ground handling and VIP services in Seychelles. Our team is experienced and will take care of all your needs while you are here.
+                                </p>
+                            </div>
+
+
+                            <div className="w-full border-b md:w-1/2 lg:w-1/3 lg:border-r p-8">
+                                <div className="flex items-center mb-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20"
+                                         fill="currentColor" className="h-6 w-6 text-emerald-500">
+                                        <path
+                                            d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z">
+                                        </path>
+                                    </svg>
+                                    <div className="ml-4 text-xl">Enterprise-ready</div>
+                                </div>
+                                <p className="leading-loose text-gray-500">Whether it’s greeting you or your clients at the airport, you can rest assured that all our guests are in the best possible hands, 24/7.
+
+                                </p>
+                            </div>
+
+
+                            <div className="w-full border-b md:w-1/2 md:border-r lg:w-1/3 lg:border-r-0 p-8">
+                                <div className="flex items-center mb-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20"
+                                         fill="currentColor" className="h-6 w-6 text-emerald-500">
+                                        <path
+                                            d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z">
+                                        </path>
+                                    </svg>
+                                    <div className="ml-4 text-xl">Unlimited growth</div>
+                                </div>
+                                <p className="leading-loose text-gray-500">With deep local knowledge, your clients will be personally handled, From private land and air charters to luxury transfers.
+                                </p>
+                            </div>
+
+
+                            <div className="w-full border-b md:w-1/2 lg:w-1/3 lg:border-r lg:border-b-0 p-8">
+                                <div className="flex items-center mb-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20"
+                                         fill="currentColor" className="h-6 w-6 text-emerald-500">
+                                        <path
+                                            d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z">
+                                        </path>
+                                    </svg>
+                                    <div className="ml-4 text-xl">Recommended by experts</div>
+                                </div>
+                                <p className="leading-loose text-gray-500"> Lofty mountains, lush green plantations, sparkling ocean water, sizzling beaches, and glittering lagoons – Seychelles has it all.
+                                </p>
+                            </div>
+
+
+                            <div
+                                className="w-full border-b md:w-1/2 md:border-r md:border-b-0 lg:w-1/3 lg:border-b-0 p-8">
+                                <div className="flex items-center mb-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20"
+                                         fill="currentColor" className="h-6 w-6 text-emerald-500">
+                                        <path
+                                            d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z">
+                                        </path>
+                                    </svg>
+                                    <div className="ml-4 text-xl">Modern platform</div>
+                                </div>
+                                <p className="leading-loose text-gray-500">With the pledge of tourist completion,
+                                    Besides admiring the fantastic landscape, there is a mind-blowing range of the best things to do in Seychelles.
+                                </p>
+                            </div>
+
+
+                            <div className="w-full md:w-1/2 lg:w-1/3 p-8">
+                                <div className="flex items-center mb-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20"
+                                         fill="currentColor" className="h-6 w-6 text-emerald-500">
+                                        <path
+                                            d="M16 3C8.8 3 3 8.8 3 16s5.8 13 13 13 13-5.8 13-13c0-1.398-.188-2.793-.688-4.094L26.688 13.5c.2.8.313 1.602.313 2.5 0 6.102-4.898 11-11 11S5 22.102 5 16 9.898 5 16 5c3 0 5.695 1.195 7.594 3.094L25 6.688C22.7 4.386 19.5 3 16 3zm11.281 4.281L16 18.563l-4.281-4.282-1.438 1.438 5 5 .719.687.719-.687 12-12z">
+                                        </path>
+                                    </svg>
+                                    <div className="ml-4 text-xl">Integrations</div>
+                                </div>
+                                <p className="leading-loose text-gray-500">With deep local knowledge, your clients will be personally handled, From private land and air charters to luxury transfers.
+                                </p>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    );
+};
+
+export default Features;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 
 import Banner from "./Banner.jsx";
+import Features from "./Features.jsx";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Features></Features>
         </div>
     );
 };
