c5af28ac44cc3597b3dd84f147167b6f377d48f4
younusFoysal <younus.faysal@gmail.com>
2024-05-05 15:36:15 +0600

    Footer Done

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Outlet} from "react-router-dom";
 import Navbar from "./Navbar.jsx";
+import Footer from "./Footer.jsx";
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
